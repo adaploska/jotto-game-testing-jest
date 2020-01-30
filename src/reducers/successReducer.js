5f4f5e29cf2e00d5105dc4fb90a4f93f36ba95a0
adaploska <adawagrocka@wp.pl>
2020-01-30 11:25:01 +0100

    set up redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @method successReducer
+ * @param {string} state - Array of guessed words.
+ * @param {string} action - action to be reduced.
+ * @returns {boolean} - new success state
+ */
+export default (state, action) => {
+  return null;
+};
