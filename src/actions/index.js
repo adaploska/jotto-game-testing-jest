d5c120b1f445e50ee4dbadebd20877e9dfb5479f
adaploska <adawagrocka@wp.pl>
2020-01-31 18:58:31 +0100

    add secret word from axios ad creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+import axios from "axios";
 import { getLetterMatchCount } from "../helpers/index";
 
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
-  GUESS_WORD: "GUESS_WORD"
+  GUESS_WORD: "GUESS_WORD",
+  SET_SECRET_WORD: "SET_SECRET_WORD"
 };
 export const guessWord = guessedWord => {
   return function(dispatch, getState) {
@@ -21,6 +23,17 @@ export const guessWord = guessedWord => {
   };
 };
 
+export const getSecretWord = () => {
+  return dispatch => {
+    const request = axios.get("http://localhost:3030");
+    return request.then(response => {
+      dispatch({
+        type: actionTypes.SET_SECRET_WORD,
+        payload: response.data
+      });
+    });
+  };
+};
 // /**
 //  * @method correctGuess
 //  * @returns {object} - action object with type CORRECT_GUESS
